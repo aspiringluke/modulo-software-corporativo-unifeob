64700cc2f3247468ec451279bb062d7285493e34
GabrielFreitas836 <gabriel.freitas@sou.unifeob.edu.br>
2025-11-03 16:00:03 -0300

    feat: utilização da middleware de checkRole para auditoria.js

--- server/routers/auditoria.js
+++ server/routers/auditoria.js
@@ -1,8 +1,9 @@
 import express from "express";
 import { log } from "../controllers/auditoria.js"
+import { checkRole } from "../middlewares/checkRole.js";
 
 const router = express.Router();
 
-router.get("/", log);
+router.get("/", checkRole('chefe'), log);
 
 export default router;
