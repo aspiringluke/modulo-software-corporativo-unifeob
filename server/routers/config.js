3d997f2205ae4595cb5fdacd8ae8954acd492749
GabrielFreitas836 <gabriel.freitas@sou.unifeob.edu.br>
2025-11-03 15:59:13 -0300

    feat: adicionar roles da sessão de usuário para config.js

--- server/routers/config.js
+++ server/routers/config.js
@@ -3,7 +3,7 @@ import express from "express";
 const router = express.Router();
 
 router.get("/", (req,res) => {
-    res.render("config");
+    res.render("config", {roles: req.session.roles});
 });
 
 export default router;
