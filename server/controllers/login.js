5cecaf0b698499e0a1245732636b0573c201d724
Lucas <l.paulino@sou.unifeob.edu.br>
2025-11-10 16:22:31 -0300

    change: Mudanças
    Lógica de verificação de senha do login alterada
    Comentários removidos
    Adicionado OrderBy decrescente nos Logs

--- server/controllers/login.js
+++ server/controllers/login.js
@@ -8,17 +8,10 @@ export async function login(req,res)
     let { usuario, senha } = req.body;
     
     try {
-        if (!senha || typeof senha !== "string" || senha.trim() === "") {
-            const senhaSalva = await lerCredenciais(usuario);
-
-            if (senhaSalva && typeof senhaSalva === "string" && senhaSalva.trim() !== "") {
-                senha = senhaSalva;
-                senha = senha.trim();
-            } else {
-                return res.render("login", { erro: "Senha não informada.", roles: null });
-            }
+        if (typeof senha !== "string" || senha.trim() === "") {
+            return res.render("login", { erro: "Senha não informada.", roles: null });
         }
-
+        
         const knex = conectar(usuario, senha);
         await knex.raw("SELECT 1;");
         knex.destroy();
@@ -29,16 +22,6 @@ export async function login(req,res)
         req.session.usuario = usuario;
         req.session.roles = roles;
 
-
-        // const cript = await encrypt(senha);
-        
-        // process.env.PSS = cript.criptografado;
-        // process.env.KEY = cript.key;
-        // process.env.IV = cript.iv;
-
-        // console.log([cript.criptografado, cript.key, cript.iv])
-        // console.log([process.env.PSS.byteLength, process.env.KEY.type, process.env.IV]);
-        
         res.render("resumos", {usuario: req.session.usuario, roles: req.session.roles});
     } catch(err) {
         res.render("login", {erro: "Usuário ou senha inválidos.", roles: null});
