c773e7d447fcf01185423d8d7845932a2a9ecf7f
GabrielFreitas836 <gabriel.freitas@sou.unifeob.edu.br>
2025-11-11 19:22:49 -0300

    feat(docs): adicionar trigger de auditoria para 'contexto' e 'vendedor'

--- docs/scriptTriggersAuditoria.sql
+++ docs/scriptTriggersAuditoria.sql
@@ -27,3 +27,15 @@ CREATE TRIGGER auditoria_produto
 AFTER INSERT OR UPDATE OR DELETE ON produto
 FOR EACH ROW
 EXECUTE FUNCTION auditoria();
+
+-- trigger 'contexto'
+CREATE TRIGGER auditoria_contexto
+AFTER INSERT OR UPDATE OR DELETE ON contexto
+FOR EACH ROW
+EXECUTE FUNCTION auditoria();
+
+-- trigger 'vendedor'
+CREATE TRIGGER auditoria_vendedor
+AFTER INSERT OR UPDATE OR DELETE ON vendedor
+FOR EACH ROW
+EXECUTE FUNCTION auditoria();
