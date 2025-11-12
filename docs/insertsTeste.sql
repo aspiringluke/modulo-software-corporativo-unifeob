c97f30180bcf626ca66f4efcbf6e80a64d1bd0bc
GabrielFreitas836 <gabriel.freitas@sou.unifeob.edu.br>
2025-11-12 14:05:33 -0300

    feat(docs): adicionar 'insertsTeste.sql'

---
+++ docs/insertsTeste.sql
@@ -0,0 +1,6 @@
+CREATE USER 'chefeTeste' WITH LOGIN PASSWORD 'chefe123';
+GRANT chefe TO 'chefeTeste';
+
+CREATE USER 'atendTeste' WITH LOGIN PASSWORD 'atend123';
+GRANT atendente TO 'atendTeste';
+
