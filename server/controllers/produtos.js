2638c1b465a88f9de0c9e7adb3b33d977e9afaa0
JoseCarlosPereiraNeto <jose.c.neto@sou.unifeob.edu.br>
2025-11-12 19:51:17 -0300

    fix: Variável Knex inicializada fora do Try-Catch

--- server/controllers/produtos.js
+++ server/controllers/produtos.js
@@ -3,10 +3,10 @@ import { lerCredenciais } from "./secureStorage.js";
 
 export async function selectAll(req,res)
 {
+    const senha = await lerCredenciais(req.session.usuario);
+    const knex = conectar(req.session.usuario, senha);
+    
     try {
-        const senha = await lerCredenciais(req.session.usuario);
-        const knex = conectar(req.session.usuario, senha);
-
         const results = await knex("produto").select(["idproduto", "descricao"]);
 
         res.status(200).json(results);
