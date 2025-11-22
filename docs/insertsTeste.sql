a11bc4d6c222e32419b59d226d61b43298da1d22
GabrielFreitas836 <gabriel.freitas@sou.unifeob.edu.br>
2025-11-22 14:45:04 -0300

    change(docs): deixar nome de usuario estritamente minúsculo considerando case-insensitive do postgre

--- docs/insertsTeste.sql
+++ docs/insertsTeste.sql
@@ -1,8 +1,8 @@
-CREATE USER chefeTeste WITH LOGIN PASSWORD 'chefe123';
-GRANT chefe TO chefeTeste;
+CREATE USER chefeteste WITH LOGIN PASSWORD 'chefe123';
+GRANT chefe TO chefeteste;
 
-CREATE USER atendTeste WITH LOGIN PASSWORD 'atend123';
-GRANT atendente TO atendTeste;
+CREATE USER atendteste WITH LOGIN PASSWORD 'atend123';
+GRANT atendente TO atendteste;
 
 INSERT INTO cliente (nome, cnpj, cidade) VALUES
 ('SP Pierin', '45667854300012', 'Santa Cruz das Palmeiras'),
